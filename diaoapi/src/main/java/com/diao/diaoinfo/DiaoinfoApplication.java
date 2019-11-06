fdc8d13b9c8b01144df6103241dbd38e48b507df
Haibing Wang <574574@qq.com>
2019-11-06 20:39:00 +0800

    new

---
+++ diaoapi/src/main/java/com/diao/diaoinfo/DiaoinfoApplication.java
@@ -0,0 +1,13 @@
+package com.diao.diaoinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaoinfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiaoinfoApplication.class, args);
+    }
+
+}
---
+++ diaoapi/src/test/java/com/diao/diaoinfo/DiaoinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diao.diaoinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaoinfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
